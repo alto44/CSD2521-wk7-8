60fe5670237ae4cca019d29c4303aa8f96ff5f73
mckeea2462 <111794966+mckeea2462@users.noreply.github.com>
2022-10-11 20:00:27 -0400

    Update HelloTest1.java
    I added a comment below line 2, then changed the comment on line 9.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Update on 10/11/2022 by AM
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
